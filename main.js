678a4cab54846b653d4b436a92943cfbf209627a
morgenmuffel1993 <lauraenriquezct@gmail.com>
2018-12-10 15:51:58 +0100

    final commit maybe

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ function main () {
   const BSectionElement = document.querySelector('.B');
   const CSectionElement = document.querySelector('.C');
   const DSectionElement = document.querySelector('.D');
+  
+  const ABoxElement = document.querySelector('.house-A');
+  const BBoxElement = document.querySelector('.house-B');
+  const CBoxElement = document.querySelector('.house-C');
+  const DBoxElement = document.querySelector('.house-D');
+  
   const buttonA = document.querySelector('.btn-A');
   const buttonB = document.querySelector('.btn-B');
   const buttonC = document.querySelector('.btn-C');
@@ -15,15 +21,19 @@ function main () {
     console.log('clicked')
     event.stopPropagation();
     ASectionElement.classList.toggle('hidden');
-
+    ABoxElement.classList.toggle('selection')
+    
     if (!BSectionElement.classList.contains('hidden')) {
       BSectionElement.classList.add('hidden');
+      BBoxElement.classList.remove('selection');
     }
     if (!CSectionElement.classList.contains('hidden')) {
       CSectionElement.classList.add('hidden');
+      CBoxElement.classList.remove('selection');
     }
     if (!DSectionElement.classList.contains('hidden')) {
       DSectionElement.classList.add('hidden');
+      DBoxElement.classList.remove('selection');
     }
 
   };
@@ -33,15 +43,19 @@ function main () {
   const showB = function (event) {
     event.stopPropagation();
     BSectionElement.classList.toggle('hidden');
+    BBoxElement.classList.toggle('selection');
 
     if (!ASectionElement.classList.contains('hidden')) {
       ASectionElement.classList.add('hidden');
+      ABoxElement.classList.remove('selection');
     }
     if (!CSectionElement.classList.contains('hidden')) {
       CSectionElement.classList.add('hidden');
+      CBoxElement.classList.remove('selection');
     }
     if (!DSectionElement.classList.contains('hidden')) {
       DSectionElement.classList.add('hidden');
+      DBoxElement.classList.remove('selection');
     }
 
   };
@@ -51,15 +65,19 @@ function main () {
   const showC = function (event) {
     event.stopPropagation();
     CSectionElement.classList.toggle('hidden');
+    CBoxElement.classList.toggle('selection');
 
     if (!ASectionElement.classList.contains('hidden')) {
       ASectionElement.classList.add('hidden');
+      ABoxElement.classList.remove('selection');
     }
     if (!BSectionElement.classList.contains('hidden')) {
       BSectionElement.classList.add('hidden');
+      BBoxElement.classList.remove('selection');
     }
     if (!DSectionElement.classList.contains('hidden')) {
       DSectionElement.classList.add('hidden');
+      DBoxElement.classList.remove('selection');
     }
 
   };
@@ -70,15 +88,19 @@ function main () {
   const showD = function (event) {
     event.stopPropagation();
     DSectionElement.classList.toggle('hidden');
+    DBoxElement.classList.toggle('selection');
 
     if (!ASectionElement.classList.contains('hidden')) {
       ASectionElement.classList.add('hidden');
+      ABoxElement.classList.remove('selection');
     }
     if (!BSectionElement.classList.contains('hidden')) {
       BSectionElement.classList.add('hidden');
+      BBoxElement.classList.remove('selection');
     }
     if (!CSectionElement.classList.contains('hidden')) {
       CSectionElement.classList.add('hidden');
+      CBoxElement.classList.remove('selection');
     }
 
   };
