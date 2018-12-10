59a93bdd6ac379c6b7650e6b4a21fef201360b21
morgenmuffel1993 <lauraenriquezct@gmail.com>
2018-12-10 14:06:18 +0100

    first-commit

---
+++ main.js
@@ -0,0 +1,89 @@
+'use strict';
+
+function main () {
+  const ASectionElement = document.querySelector('.A');
+  const BSectionElement = document.querySelector('.B');
+  const CSectionElement = document.querySelector('.C');
+  const DSectionElement = document.querySelector('.D');
+  const buttonA = document.querySelector('.btn-A');
+  const buttonB = document.querySelector('.btn-B');
+  const buttonC = document.querySelector('.btn-C');
+  const buttonD = document.querySelector('.btn-D');
+
+  const showA = function (event) {
+    event.stopPropagation();
+    ASectionElement.classList.toggle('hidden');
+
+    if (!BSectionElement.classList.contains('hidden')) {
+      BSectionElement.classList.add('hidden');
+    }
+    if (!CSectionElement.classList.contains('hidden')) {
+      CSectionElement.classList.add('hidden');
+    }
+    if (!DSectionElement.classList.contains('hidden')) {
+      DSectionElement.classList.add('hidden');
+    }
+
+  };
+
+  buttonA.addEventListener('click', showA);
+
+  const showB = function (event) {
+    event.stopPropagation();
+    BSectionElement.classList.toggle('hidden');
+
+    if (!ASectionElement.classList.contains('hidden')) {
+      ASectionElement.classList.add('hidden');
+    }
+    if (!CSectionElement.classList.contains('hidden')) {
+      CSectionElement.classList.add('hidden');
+    }
+    if (!DSectionElement.classList.contains('hidden')) {
+      DSectionElement.classList.add('hidden');
+    }
+
+  };
+
+  buttonB.addEventListener('click', showB);
+
+  const showC = function (event) {
+    event.stopPropagation();
+    CSectionElement.classList.toggle('hidden');
+
+    if (!ASectionElement.classList.contains('hidden')) {
+      ASectionElement.classList.add('hidden');
+    }
+    if (!BSectionElement.classList.contains('hidden')) {
+      BSectionElement.classList.add('hidden');
+    }
+    if (!DSectionElement.classList.contains('hidden')) {
+      DSectionElement.classList.add('hidden');
+    }
+
+  };
+
+  buttonC.addEventListener('click', showC);
+
+
+  const showD = function (event) {
+    event.stopPropagation();
+    DSectionElement.classList.toggle('hidden');
+
+    if (!ASectionElement.classList.contains('hidden')) {
+      ASectionElement.classList.add('hidden');
+    }
+    if (!BSectionElement.classList.contains('hidden')) {
+      BSectionElement.classList.add('hidden');
+    }
+    if (!CSectionElement.classList.contains('hidden')) {
+      CSectionElement.classList.add('hidden');
+    }
+
+  };
+
+  buttonD.addEventListener('click', showD);
+
+
+
+}
+window.addEventListener('load', main);
